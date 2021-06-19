479103577c5191276e9b6fe65029530f4adfd581
Diego Pego <diegodamascenopego@hotmail.com>
2021-06-19 20:20:55 -0300

    adicionado nome

--- src/main/java/com/homework/domain/core/animal/model/Animal.java
+++ src/main/java/com/homework/domain/core/animal/model/Animal.java
@@ -1,12 +1,13 @@
 package com.homework.domain.core.animal.model;
 
 import com.homework.domain.shared.Descricao;
+import com.homework.domain.shared.Nome;
 
 public class Animal {
 
-    private final Descricao descricao;
+    private final Nome nome;
 
-    public Animal(Descricao descricao) {
-        this.descricao = descricao;
+    public Animal(Nome nome) {
+        this.nome = nome;
     }
 }
